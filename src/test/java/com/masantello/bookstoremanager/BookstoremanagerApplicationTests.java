f37d80875d71983ca881efac1e5015c6b486a0eb
Marcely Biguzzi Santello <marcely@sbsoftware.com.br>
2026-02-19 00:03:34 -0300

    Project BookstoreManager created

---
+++ src/test/java/com/masantello/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masantello.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
